547769648b8569a14431ae9f0b2400c124e366fb
Matt Schafer <lucidsigma17@gmail.com>
2020-04-12 13:59:40 +1000

    Finish skills section on home page.

--- public/scripts/resize.js
+++ public/scripts/resize.js
@@ -1,7 +1,7 @@
-$("#meshTerrain").outerHeight($("#biography").outerHeight());
-$("#skills").outerHeight($("#skillsInfo").outerHeight());
-	
-$(window).resize(() => {
+const resizeCanvases = () => {
 	$("#meshTerrain").outerHeight($("#biography").outerHeight());
 	$("#skills").outerHeight($("#skillsInfo").outerHeight());
-});
+};
+
+$(document).ready(resizeCanvases);
+$(window).resize(resizeCanvases);
